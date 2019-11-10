56138dade398af471fe4c803c7e7aa3fca5e954a
Guan-Smile <521094300@qq.com>
2019-11-10 11:46:21 +0800

    简易结果显示版

--- guan_code/parzen.m
+++ guan_code/parzen.m
@@ -1,11 +1,11 @@
 function r=parzen(w,h,x)
-[rows,dimension]=size(w);
+[rows,~,dimension]=size(w);  %w = [样本数目，169维特征，10类分类]
 r=zeros(1,dimension);
 for i=1:dimension
     hn=h;
     for j=1:rows
         hn=hn/sqrt(j);
-        r(i)=r(i)+exp(-(x-w(j,:,i))*(x-w(j,:,i))'/(2*power(hn,2)))/(sqrt(2*3.14)*hn);
+        r(i)=r(i)+exp(-(x-w(j,:,i))*(x-w(j,:,i))'/(2*power(hn,2)))/(sqrt(2*pi)*hn);%% 正态窗。
     end
     r(i)=r(i)/rows;
 end
